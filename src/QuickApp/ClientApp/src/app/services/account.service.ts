c42f15ee47eb7be4d2b3f8a763cc8afeade0bffb
Ciaran <ciaran08@gmail.com>
2018-12-15 00:38:29 +0000

    Test change in VS

--- src/QuickApp/ClientApp/src/app/services/account.service.ts
+++ src/QuickApp/ClientApp/src/app/services/account.service.ts
@@ -1,8 +1,3 @@
-// ====================================================
-// More Templates: https://www.ebenmonney.com/templates
-// Email: support@ebenmonney.com
-// ====================================================
-
 import { Injectable } from '@angular/core';
 import { Router, NavigationExtras } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
